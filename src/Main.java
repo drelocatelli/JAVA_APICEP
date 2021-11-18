e075097c9d3778519c3e2582aa0bda869a7b86f1
4ndr3224 <32282846+drelocatelli@users.noreply.github.com>
2021-11-17 22:41:48 -0300

    Codigo novo

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,48 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import netscape.javascript.JSObject;
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Scanner;
 
-import org.json.*;
-
-public class Main {
+public class Program {
 	public static Scanner input = new Scanner(System.in);
 	public static String cep;
-	public static String method = "GET";
 	public static String webservice;
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
+
 		System.out.print("Digite o CEP: ");
 		cep = input.nextLine();
 		System.out.println("CEP BUSCADO: "+ cep);
-		
+
 		webservice = "https://viacep.com.br/ws/" + cep + "/json";
 
-			try {
-	
-	            // conexao
-	            URL url = new URL(webservice);
-	            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-	            conn.setRequestMethod(method);
-	            conn.setRequestProperty("Accept", "application/json");
-	
-	            if (conn.getResponseCode() != 200)
-	                throw new RuntimeException("HTTP ERROR CODE " + conn.getResponseCode());
-	
-	            // Input
-	            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-	            StringBuilder result = new StringBuilder();
-	            String output;
-	            
-	            while((output = br.readLine()) != null){
-	                result.append(output);
-	            }
-	            
-	            JSONObject response = new JSONObject(result.toString());
-	            
-//	            System.out.println(response);
-	            System.out.printf(
-	            		"Logradouro: %s \n"
-	            		+ "Bairro: %s \n"
-	            		+ "Localidade: %s-%s \n"
-	            		, response.get("logradouro"), response.get("bairro"), response.get("localidade"), response.get("uf"));
+		try {
+			HttpRequest request = HttpRequest.newBuilder()
+					.uri(new URI(webservice))
+					.GET()
+					.build();
+
+			HttpClient httpClient = HttpClient.newHttpClient();
+			var data = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+			JSONObject response = new JSONObject(data.body());
+
+			System.out.printf(
+					"Logradouro: %s \n"
+							+ "Bairro: %s \n"
+							+ "Localidade: %s-%s \n"
+					, response.get("logradouro"), response.get("bairro"), response.get("localidade"), response.get("uf"));
 
 
-				conn.disconnect();
-			
-			}catch(Exception err) {
-				err.printStackTrace();
-			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+
 	}
 
 }
